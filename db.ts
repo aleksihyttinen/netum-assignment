6584917f63bde3e862f4eadc72d500f773d27c2e
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-02-10 10:40:36 +0200

    Add connection pool and getUsers function

---
+++ db.ts
@@ -0,0 +1,26 @@
+import mysql from "mysql2";
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const connectionFunctions = {
+  getUsers: () => {
+    return new Promise((resolve, reject) => {
+      pool.query(`SELECT * FROM users`, (err: Error, result: []) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(result);
+      });
+    });
+  },
+};
+module.exports = connectionFunctions;

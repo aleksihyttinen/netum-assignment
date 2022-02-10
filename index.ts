749b3fc976f1df0a9642eeac08c89387401edbef
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-02-10 10:39:02 +0200

    Add function for get all users

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+const connection = require("./db.ts");
+
+app.get("/users", async (req: express.Request, res: express.Response) => {
+  try {
+    let data = await connection.getUsers();
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.statusCode = 400;
+    res.end();
+  }
+});
+const port: string | number = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});

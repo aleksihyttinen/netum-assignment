aff66071c1a106bbe918ed313e181fe4bab3fda6
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-02-11 09:39:33 +0200

    Add table which displays all data from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import { Table } from "react-bootstrap";
 function App() {
   const [users, setUsers] = React.useState([]);
   React.useEffect(() => {
@@ -9,7 +10,30 @@ function App() {
     });
   }, []);
   console.log(users);
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="Table-container">
+        <Table bordered>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user: any) => (
+              <tr key={user.id}>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+                <td>{user.age}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
 }
 
 export default App;

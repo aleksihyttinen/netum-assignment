89152efb843ab9c34b175d1f737c7e42796a87cf
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-02-17 14:05:49 +0200

    Add interface ISortConfig and add id to IUser

--- frontend/src/Interfaces.ts
+++ frontend/src/Interfaces.ts
@@ -1,5 +1,10 @@
 export interface IUser {
+  id?: number;
   first_name: string;
   last_name: string;
   age?: number;
+}
+export interface ISortConfig {
+  sortBy: string;
+  direction?: string;
 }

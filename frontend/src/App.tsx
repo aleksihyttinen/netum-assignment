324b05f7e0558e733cce2e3ad251c0766ac43260
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-02-17 14:06:36 +0200

    Add ability to sort arrays based on a column

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,21 @@ import { Table } from "react-bootstrap";
 import EditUser from "./EditUser";
 import AddUser from "./AddUser";
 import DeleteUser from "./DeleteUser";
+import { IUser, ISortConfig } from "./Interfaces";
+import sortArray from "./sortArray";
 function App() {
-  const [users, setUsers] = React.useState([]);
+  const [users, setUsers] = React.useState<IUser[]>([]);
+  const [sortedUsers, setSortedUsers] = React.useState<IUser[]>([]);
   const [edited, setEdited] = React.useState(false);
+  const [sortConfig, setSortConfig] = React.useState<ISortConfig>({
+    sortBy: "",
+    direction: "default",
+  });
+
+  React.useEffect(() => {
+    sortArray({ users, sortConfig, setSortedUsers });
+  }, [sortConfig, users]);
+
   React.useEffect(() => {
     axios
       .get("/users")
@@ -17,7 +29,20 @@ function App() {
       })
       .catch((err) => console.log(err));
   }, [edited]);
-  console.log(users);
+
+  const getSortIcon = (column: string) => {
+    if (sortConfig.sortBy === column) {
+      if (sortConfig.direction === "ascending") {
+        return "⬆";
+      }
+      if (sortConfig.direction === "decending") {
+        return "⬇";
+      }
+      return " ";
+    }
+    return " ";
+  };
+
   return (
     <div className="App">
       <AddUser setEdited={setEdited} />
@@ -25,24 +50,81 @@ function App() {
         <Table bordered>
           <thead>
             <tr>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Age</th>
+              <th
+                className="first-name"
+                onClick={() => {
+                  setSortConfig({
+                    sortBy: "first_name",
+                    direction:
+                      sortConfig.direction === "default"
+                        ? "ascending"
+                        : sortConfig.direction === "ascending"
+                        ? "decending"
+                        : "default",
+                  });
+                }}
+              >
+                First Name {getSortIcon("first_name")}
+              </th>
+              <th
+                className="last-name"
+                onClick={() => {
+                  setSortConfig({
+                    sortBy: "last_name",
+                    direction:
+                      sortConfig.direction === "default"
+                        ? "ascending"
+                        : sortConfig.direction === "ascending"
+                        ? "decending"
+                        : "default",
+                  });
+                }}
+              >
+                Last Name {getSortIcon("last_name")}
+              </th>
+              <th
+                className="age"
+                onClick={() => {
+                  setSortConfig({
+                    sortBy: "age",
+                    direction:
+                      sortConfig.direction === "default"
+                        ? "ascending"
+                        : sortConfig.direction === "ascending"
+                        ? "decending"
+                        : "default",
+                  });
+                }}
+              >
+                Age {getSortIcon("age")}
+              </th>
               <th>Settings</th>
             </tr>
           </thead>
           <tbody>
-            {users.map((user: any) => (
-              <tr key={user.id}>
-                <td>{user.first_name}</td>
-                <td>{user.last_name}</td>
-                <td>{user.age}</td>
-                <td>
-                  <EditUser user={user} setEdited={setEdited} />
-                  <DeleteUser user={user} setEdited={setEdited} />
-                </td>
-              </tr>
-            ))}
+            {sortedUsers.length !== 0
+              ? sortedUsers.map((user: IUser) => (
+                  <tr key={user.id}>
+                    <td>{user.first_name}</td>
+                    <td>{user.last_name}</td>
+                    <td>{user.age}</td>
+                    <td>
+                      <EditUser user={user} setEdited={setEdited} />
+                      <DeleteUser user={user} setEdited={setEdited} />
+                    </td>
+                  </tr>
+                ))
+              : users.map((user: IUser) => (
+                  <tr key={user.id}>
+                    <td>{user.first_name}</td>
+                    <td>{user.last_name}</td>
+                    <td>{user.age}</td>
+                    <td>
+                      <EditUser user={user} setEdited={setEdited} />
+                      <DeleteUser user={user} setEdited={setEdited} />
+                    </td>
+                  </tr>
+                ))}
           </tbody>
         </Table>
       </div>
